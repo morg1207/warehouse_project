8581698f5364a1a20ffa95c6382cad65e3eb4dc1
morg1207 <alaurao@uni.pe>
2023-06-04 18:12:32 -0500

    pkg- path_planner_server -- Launch - pathplanner.launch.py -- function: configurate path planner nav2 -- version : 1.0

---
+++ path_planner_server/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+from glob import glob
+package_name = 'path_planner_server'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='morg1207',
+    maintainer_email='alaurao@uni.pe',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
