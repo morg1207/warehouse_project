b8a499050cec13201f7e26057b5701e6b85078d3
morg1207 <alaurao@uni.pe>
2023-06-04 18:17:59 -0500

    Create : pkg- map_server-- Launch - map_serverlaunch.py -- function: add maps -- version : 2.0

--- map_server/launch/map_server.launch.py
+++ map_server/launch/map_server.launch.py
@@ -3,38 +3,27 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration,PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, PythonExpression
 
 def generate_launch_description():
-    print('Init')
     package_name = 'map_server'
-    config_directory = os.path.join(get_package_share_directory(package_name), 'config')
     rviz_file = os.path.join(get_package_share_directory(package_name),'rviz','rviz_config.rviz')
-    print('Init2 ')
     #~~~~~~~~~~~~~~~~~~Declare parameters~~~~~~~~~~~~~++
     # Declara el argumento para el archivo de configuración YAML
-    config_file = DeclareLaunchArgument(
+    map_file = LaunchConfiguration('map_file')
+    arg_map_file = DeclareLaunchArgument(
         'map_file',
-        default_value='warehouse_map_real.yaml',
+        default_value='warehouse_map_sim.yaml',
         description='Path to the map select'
     )
-    config_file =  LaunchConfiguration('map_file')
     print('Init3')
-    print(config_directory)
-    # Define la ruta completa del archivo de mapa
-    map_file_path = os.path.join(str(PathJoinSubstitution(config_file)),'dasd')
+    # Obtener la ruta completa del archivo YAML del mapa
+    package_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-    print(map_file_path)
-    print('Init3')
-        # Imprime un comentario
-    print(map_file_path)
-    # Verifica si el archivo de mapa existe
-    #check_map_file = launch.actions.OpaqueFunction(function=lambda context: print(f"Checking if map file '{map_file_path}' exists..."))
-
+    map_file_path = PythonExpression(["'",package_path, "/config","/", map_file, "'"])
 
     return LaunchDescription([
-        config_file,
-        #check_map_file,    
+        arg_map_file,
 
         #~~~~~~~~~~~~~~~~~~provide map~~~~~~~~~~~~~~~~~~~~~~~~~~
         Node(
